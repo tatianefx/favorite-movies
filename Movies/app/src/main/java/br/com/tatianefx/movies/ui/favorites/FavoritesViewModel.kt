4d2778370ac1ea7cf87b9ff98c58c377c1a80608
Tatiane Souza <tatianefx@gmail.com>
2019-03-15 15:12:59 -0300

    Fix favorite movies

--- Movies/app/src/main/java/br/com/tatianefx/movies/ui/favorites/FavoritesViewModel.kt
+++ Movies/app/src/main/java/br/com/tatianefx/movies/ui/favorites/FavoritesViewModel.kt
@@ -72,6 +72,7 @@ class FavoritesViewModel(private val moviesRepository: MoviesRepository): Movies
     //region Override Methods
 
     override fun onMoviesLoaded(movies: List<Movie>) {
+        items.clear()
         items.addAll(movies)
         _adapter.value?.notifyDataSetChanged()
         _noFavoriteMoviesVisibility.value = View.GONE
